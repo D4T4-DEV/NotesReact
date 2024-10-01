e90d13f69fc666da7111fdb669b426fa8baabd29
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-01 17:09:32 -0600

    Enlace de google para iconos

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Enlace de iconos de google*/}
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <App />
   </StrictMode>,
 )
