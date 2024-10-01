2a22f914fd01ae18b770c4a4fedbaff5d4c76251
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-01 17:44:58 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { DndContext } from '@dnd-kit/core'
 import AppBarComponent from './Components/AppBarComponent'
 
 function App() {
+
+
+
   return (
     <div>
+
+      <DndContext
+      // Componentes 
+      >
+
+      </DndContext>
+
       {/* Componente de AppBar */}
-      <AppBarComponent/>
-      
+      <AppBarComponent />
+
     </div>
   )
 }
