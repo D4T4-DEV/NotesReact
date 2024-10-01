cc21a46bcd49edac8c9af029f27815d0a989c7a3
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-01 17:16:19 -0600

    Utilizacion del contexto en la aplicacion

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ContainerProvider } from './Contexts/AppContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* Enlace de iconos de google*/}
-     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <App />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <ContainerProvider>
+      <App />
+    </ContainerProvider>
   </StrictMode>,
 )
