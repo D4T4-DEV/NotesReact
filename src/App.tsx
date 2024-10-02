9fda58a6b067ef9989d8a8b41066630e3141aae8
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-01 21:08:51 -0600

    Hacemos el componente APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,129 @@
-import { DndContext } from '@dnd-kit/core'
+import { closestCenter, DndContext, KeyboardSensor, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
 import AppBarComponent from './Components/AppBarComponent'
+import { useContainerContext } from './Contexts/AppContext';
+import { useState } from 'react';
+import AddItem from './Components/AddItem';
+import Container from './Components/ContenedorDeNotas';
+import Card from './Components/Nota';
 
 function App() {
 
+  // Uso de un estado para saber el contenedor que se esta movimiento un item
+  const [activeContainerId, setActiveContainerId] = useState<string | null>(null);
 
+  // Uso del contexto
+  const { containers, dispatch } = useContainerContext();
+
+
+  // Codigo afin a dnd-kit
+
+  const sensors = useSensors( // Sensor para habilitar la entrada por teclado y manejar su comportamiento
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 5,
+      },
+    }),
+    useSensor(KeyboardSensor)
+  );
+
+  // Funcion para detectar cunado se termina de arrastrar un item hacia algun lado
+  const handleDragEnd = (event: any) => {
+    const { active, over } = event;
+    if (!over) return;
+
+    setActiveContainerId(null);
+
+    const activeType = active.data.current?.type;
+    const overType = over.data.current?.type;
+
+    if (activeType === 'item' && overType === 'container') {
+      const sourceIndex = containers.findIndex((container) =>
+        container.items.some(item => item.id === active.id)
+      );
+      const destinationIndex = containers.findIndex(container => container.id === over.id);
+
+      if (sourceIndex !== -1 && destinationIndex !== -1 && sourceIndex !== destinationIndex) {
+        // Uso del contexto para mover el item entre contenedores
+        dispatch({
+          type: 'MOVE_ITEM_BETWEEN_CONTAINERS',
+          payload: { sourceIndex, destinationIndex, itemId: active.id },
+        });
+      }
+    }
+
+    if (activeType === 'item' && overType === 'item') {
+      const containerIndex = containers.findIndex(container =>
+        container.items.some(item => item.id === active.id)
+      );
+
+      if (containerIndex !== -1) {
+        const activeIndex = containers[containerIndex].items.findIndex(item => item.id === active.id);
+        const overIndex = containers[containerIndex].items.findIndex(item => item.id === over.id);
+
+        if (activeIndex !== overIndex) {
+          // Uso del contexto para mover el item en el mismo contenedor
+          dispatch({
+            type: 'MOVE_ITEM_WITHIN_CONTAINER',
+            payload: { containerIndex, activeIndex, overIndex },
+          });
+        }
+      }
+    }
+  };
+
+  // Funcion para saber el tipo de item que se tiene cuando se suelta otro
+  const handleDragOver = (event: any) => {
+    const { over } = event;
+    if (over && over.data.current?.type === 'container') {
+      setActiveContainerId(over.id);
+    } else {
+      setActiveContainerId(null);
+    }
+  };
 
   return (
     <div>
 
-      <DndContext
-      // Componentes 
-      >
-
-      </DndContext>
-
       {/* Componente de AppBar */}
       <AppBarComponent />
 
+      {/* Componente de dnd-kit, el cual usa su propio contexto */}
+      <DndContext
+        // Componentes 
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+        onDragOver={handleDragOver}
+      >
+        <div className='app'>
+          {/* Componente encargado de agregar notas al contenedor padre */}
+          <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <AddItem containerId={'father-items-god'} />
+          </div>
+
+          {containers.map((container) => (
+          <Container
+            key={container.id}
+            id={container.id}
+            items={container.items}
+            isActive={container.id === activeContainerId}
+            type="container"
+          >
+            {container.items.map((item) => (
+              <Card
+                key={item.id}
+                id={item.id}
+                type="item"
+                title={item.title}
+                message={item.message}
+                colorItem={item.color}
+                containerId={container.id}
+              />
+            ))}
+          </Container>
+        ))}
+        </div>
+      </DndContext>
     </div>
   )
 }
