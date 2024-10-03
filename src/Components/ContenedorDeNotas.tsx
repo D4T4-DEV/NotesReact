b8dfb57a46e9a9ce4722d19159dcde6fce1ce3d4
D4T4 <149961434+D4T4-DEV@users.noreply.github.com>
2024-10-03 08:57:49 -0600

    Update ContenedorDeNotas.tsx

--- src/Components/ContenedorDeNotas.tsx
+++ src/Components/ContenedorDeNotas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDroppable } from '@dnd-kit/core';
 import { horizontalListSortingStrategy, SortableContext } from '@dnd-kit/sortable';
 import { Button } from '@mui/material';
