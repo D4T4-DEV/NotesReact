9d9d2ee657836e6b34d416e4c10bb8e3b33c64ba
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-02 23:42:42 -0600

    Cambios de nombre de usuario
    Cambios para que para cada accion no cambie el nombre de usuario

--- src/Components/AppBarComponent.tsx
+++ src/Components/AppBarComponent.tsx
@@ -1,15 +1,20 @@
+import { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import getUserRandom from '../Functions/Aleatorios/getUser';
 
-
 const AppBarComponent = () => {
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        setUsername(getUserRandom());
+    }, []); // Solo se ejecuta cuando se renderiza este componente
 
     return (
         <Box sx={{ flexGrow: 1, mb: 2 }}>
-            <AppBar position="static" sx={{ backgroundColor: '#606676', borderRadius: 1.5, margin: 0, padding: '20px', fontStyle: 'normal' }} >
+            <AppBar position="static" sx={{ backgroundColor: '#606676', borderRadius: 1.5, margin: 0, padding: '20px', fontStyle: 'normal' }}>
                 <Toolbar>
                     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                         <Typography
@@ -21,15 +26,14 @@ const AppBarComponent = () => {
                         </Typography>
 
                         <Typography
-                            sx={{ fontSize: '1.5em', fontWeight: 400, fontFamily: 'Noto Sans, sans-serif' , color: '#FFFF', fontStyle: 'normal' }}>
-                                Bienvenido {getUserRandom()}
+                            sx={{ fontSize: '1.5em', fontWeight: 400, fontFamily: 'Noto Sans, sans-serif', color: '#FFFF', fontStyle: 'normal' }}>
+                                Bienvenido {username}, espero te agrade mi aplicación de notas!
                         </Typography>
                     </Box>
-
                 </Toolbar>
             </AppBar>
         </Box>
     );
 }
 
-export default AppBarComponent
+export default AppBarComponent;
